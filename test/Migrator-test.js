c064d1cfe99f26f7dd2ae3c6032e259068abd45e
zigur <massimorgon@gmail.com>
2015-06-17 11:30:43 +0200

    added tests #2

---
+++ test/Migrator-test.js
@@ -0,0 +1,14 @@
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+var Migrator = require('./../lib/Migrator.js');
+
+describe('Migrator', function() {
+    
+    describe('#constructor', function() {
+       
+        var migrator = new Migrator();
+
+    });
+
+});
