2ee3fc05387f9705e55d8b165e713bbadf38195b
zigur <massimorgon@gmail.com>
2015-12-14 00:45:02 +0100

    microarray and file migration now supported

--- migrate.js
+++ migrate.js
@@ -1,12 +1,15 @@
 #! /usr/bin/env node
 
+var logger = require('./logger.js');
 var Migrator = require("./lib/Migrator.js");
 console.log("Creating new migrator");
 var migrator = new Migrator();
+//migrator.migrateCGH("/home/massi/Projects/aCGH/FileBIT",".xlsx")
 migrator.migrateAllSubjects()
+// migrator.migrateCompleteSubject(156)
 .then(function() {
-    console.log("Done!");
+    logger.log('info', 'migrate: done!');
 })
 .catch(function(err) {
-    console.log(err && err.message);
+    logger.log('error', err);
 });
