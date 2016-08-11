c9025f5caccb2e2ffeb9f5ae35f7482ac8596f81
gicaz <nico.zanardi@gmail.com>
2016-08-11 11:43:19 +0200

    Added updating NB Clinical Info

--- config/connections.js
+++ config/connections.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    bearerToken: 'eyJhbGciOiJIUzI1NiJ9.MQ.Lu-KcR4aCeuT9hi1K474zV3s4VaopLDCcf4nZvH6DQo',
+    bearerToken: 'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6NywiaXNXaGVlbCI6dHJ1ZSwiaXNBZG1pbiI6dHJ1ZSwiY2FuQWNjZXNzUGVyc29uYWxEYXRhIjp0cnVlLCJjYW5BY2Nlc3NTZW5zaXRpdmVEYXRhIjp0cnVlfQ.Tf57NjL4LJ1iHqrYfO9KzBYlNbjUBDEK00uR2YlazQg',
 
     mysqlLocal: {
         client: 'mysql',
---
+++ updateCBInfo.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+var logger = require('./logger.js');
+var Migrator = require("./lib/Migrator.js");
+logger.log('info', "Creating new migrator");
+var migrator = new Migrator();
+//migrator.migrateCGH("/home/massi/Projects/aCGH/FileBIT",".xlsx")
+return migrator.importCBInfo('/var/xtens/dataFiles/tmp','.xlsx')
+//migrator.migrateCompleteSubject(5)
+.then(function() {
+    logger.log('info', 'migrate: done!');
+    process.exit(0);
+})
+.catch(function(err) {
+    logger.log('error', err);
+    process.exit(1);
+});
