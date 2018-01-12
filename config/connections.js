34bff854c8d0a345b5190b123e36bd757e85ad8b
gicaz <nico.zanardi@gmail.com>
2018-01-12 15:46:08 +0100

    Added Variant Annotation Db default connection

--- config/connections.js
+++ config/connections.js
@@ -21,6 +21,17 @@ module.exports = {
             password: 'xtenspg',
             database: 'xtensigg'
         }
+    },
+
+    postgresqlLocalAnnotiation: {
+        client: 'pg',
+        connection: {
+            host: '127.0.0.1',
+            port: 5432,
+            user: 'xtenspg',
+            password: 'xtenspg',
+            database: 'xtens_genomic_annotation'
+        }
     }
 
 };
