a17dcc989414261f7043af15d80aeaa4834d93a0
gicaz <nico.zanardi@gmail.com>
2018-12-21 11:49:15 +0100

    script migrateBioAn

---
+++ createBIOAN.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+var logger = require('./logger.js');
+var Migrator = require("./lib/Migrator.js");
+logger.log('info', "Creating new migrator");
+var migrator = new Migrator();
+var DEFAULT_LOCATION = '../xtens-app/assets/dataFiles/tmp';
+
+migrator.migrateBioAn(DEFAULT_LOCATION,'.vcf', process)
+.then(function(summary) {
+    logger.log('info', 'migrate: done!');
+    process.send(summary, function () {
+        if (summary.error) {
+            process.exit(1);
+        }
+        process.exit(0);
+    });
+
+})
+.catch(function(err) {
+    logger.log('error', err);
+    process.exit(1);
+});
