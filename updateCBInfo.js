67af41ff590dc978937444e9d82c1a5df9edd3e3
gicaz <nico.zanardi@gmail.com>
2019-05-24 17:00:21 +0200

    self signed cert

--- updateCBInfo.js
+++ updateCBInfo.js
@@ -5,17 +5,18 @@ var Migrator = require("./lib/Migrator.js");
 logger.log('info', "Creating new migrator");
 var migrator = new Migrator();
 var DEFAULT_LOCATION = '../xtens-app/assets/dataFiles/tmp';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-return migrator.importCNBInfo(DEFAULT_LOCATION,'.xlsx')
-//migrator.migrateCompleteSubject(5)
-.then(function(summary) {
+return migrator.importCNBInfo(DEFAULT_LOCATION, '.xlsx')
+  //migrator.migrateCompleteSubject(5)
+  .then(function (summary) {
     logger.log('info', 'migrate: done!');
-    process.send(summary,function () {
+    process.send(summary, function () {
       process.exit(0);
     });
 
-})
-.catch(function(err) {
+  })
+  .catch(function (err) {
     logger.log('error', err);
     process.exit(1);
-});
+  });
