271288cf0fe0546927b2f04d03f68060ee487f85
gicaz <nico.zanardi@gmail.com>
2021-01-21 17:56:22 +0100

    moved path logger

--- logger.js
+++ logger.js
@@ -31,8 +31,8 @@ var winston = require('winston');
         // - Write all logs with level `error` and below to `error.log`
         // - Write all logs with level `info` and below to `combined.log`
         //
-        new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: './logs/combined.log', level: 'info' }),
+        new winston.transports.File({ filename: './logs/MigrateUtils/error.log', level: 'error' }),
+        new winston.transports.File({ filename: './logs/MigrateUtils/combined.log', level: 'info' }),
       ],
     });
     
