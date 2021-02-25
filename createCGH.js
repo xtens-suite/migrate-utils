48675c9840332a5eb1fe6552e71b43014b4d6ba2
gicaz <nico.zanardi@gmail.com>
2021-02-25 17:12:41 +0100

    fix ngs pat and analyses import

--- createCGH.js
+++ createCGH.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-var loggerGen = require('./../logger.js');
+var loggerGen = require('./logger.js');
 const logger = loggerGen();
 var Migrator = require("./lib/Migrator.js");
 logger.log('info', "Creating new migrator");
