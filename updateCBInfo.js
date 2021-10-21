95b9620e0514c91489fea8388318574cd7e4d493
ordet76 <robertocavanna@gaslini.org>
2021-10-21 15:15:55 +0200

    fixed an issue on CBInfo import

--- updateCBInfo.js
+++ updateCBInfo.js
@@ -5,7 +5,7 @@ const logger = loggerGen();
 var Migrator = require("./lib/Migrator.js");
 logger.log('info', "Creating new migrator");
 var migrator = new Migrator();
-var DEFAULT_LOCATION = '../xtens-file-system/landing'; //'/var/www/xtens-app/assets/dataFiles/tmp' '../xtens-app/assets/dataFiles/tmp' '../xtens-file-system/landing'
+var DEFAULT_LOCATION = '/var/www/xtens-app/assets/dataFiles/tmp'; //'/var/www/xtens-app/assets/dataFiles/tmp' '../xtens-app/assets/dataFiles/tmp' '../xtens-file-system/landing'
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 return migrator.importCNBInfo(DEFAULT_LOCATION, '.xlsx')
